fa0546b7c4b491965f552cbffe848c8513668d75
Ross Zurowski <ross@rosszurowski.com>
2018-03-25 11:23:09 -0700

    Show series URL for all sites that link out

--- src/components/series-row.js
+++ src/components/series-row.js
@@ -20,37 +20,42 @@ const SeriesRow = ({
   linkToUrl,
   onOptionsClick,
   onSeriesClick,
-}: Props) => (
-  <div className="SeriesRow x bb-1 bc-lightGray bc-transparent-m">
-    <a
-      href={linkToUrl ? linkToUrl : series.url}
-      onClick={onSeriesClick(series.id)}
-      target="_blank"
-      className="hover x-1 x xd-column ph-3 pv-3">
-      <span className="fs-20-m">
-        {isUnread && (
-          <span className="p-relative t--2 mr-2">
-            <span className="d-inlineBlock w-8 h-8 br-round bgc-pink" />
+}: Props) => {
+  const readingUrl = linkToUrl ? linkToUrl : series.url;
+  const usesExternalReader = linkToUrl || series.supportsReading === false;
+
+  return (
+    <div className="SeriesRow x bb-1 bc-lightGray bc-transparent-m">
+      <a
+        href={readingUrl}
+        onClick={onSeriesClick(series.id)}
+        target="_blank"
+        className="hover x-1 x xd-column ph-3 pv-3">
+        <span className="fs-20-m">
+          {isUnread && (
+            <span className="p-relative t--2 mr-2">
+              <span className="d-inlineBlock w-8 h-8 br-round bgc-pink" />
+            </span>
+          )}
+          <span className={isUnread ? 'fw-medium' : undefined}>
+            {series.title}
           </span>
-        )}
-        <span className={isUnread ? 'fw-medium' : undefined}>
-          {series.title}
         </span>
-      </span>
-      <span className="fs-12 o-50p">
-        {linkToUrl && (
-          <Fragment>
-            {utils.getDomainName(linkToUrl)}
-            <span className="fs-9 ph-1 p-relative t--1">&bull;</span>
-          </Fragment>
-        )}
-        {utils.formatTimestamp(series.updatedAt)}
-      </span>
-    </a>
-    <button className="pa-3" onClick={onOptionsClick(series.id)}>
-      <IconMoreHorizontal />
-    </button>
-  </div>
-);
+        <span className="fs-12 o-50p">
+          {usesExternalReader && (
+            <Fragment>
+              {utils.getDomainName(readingUrl)}
+              <span className="fs-9 ph-1 p-relative t--1">&bull;</span>
+            </Fragment>
+          )}
+          {utils.formatTimestamp(series.updatedAt)}
+        </span>
+      </a>
+      <button className="pa-3" onClick={onOptionsClick(series.id)}>
+        <IconMoreHorizontal />
+      </button>
+    </div>
+  );
+};
 
 export default SeriesRow;
