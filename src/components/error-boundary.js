362098bcacd71b3e3b68baf8ad44cc9e5315f950
Ross Zurowski <ross@rosszurowski.com>
2018-04-04 09:28:42 -0700

    Fix link

--- src/components/error-boundary.js
+++ src/components/error-boundary.js
@@ -39,7 +39,7 @@ export default class ErrorBoundary extends Component<Props, State> {
             <button className="Link" onClick={this.handleReloadClick}>
               reload the page
             </button>, or help us by{' '}
-            <a href="https://github.com/rosszurowski/poketo-site/issues/new">
+            <a href="https://github.com/poketo/site/issues/new">
               reporting this issue
             </a>{' '}
             with the error below.
