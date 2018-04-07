2c5c7a25e4cbe133c0444cd6d6ff4982afcd5014
Ross Zurowski <ross@rosszurowski.com>
2018-04-07 14:14:14 -0700

    Tweak homepage

--- src/views/home-view.js
+++ src/views/home-view.js
@@ -1,9 +1,9 @@
 // @flow
 
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import Button from '../components/button';
-import Footer from '../components/footer';
 import poketoIntroUrl from '../assets/poketo-intro.svg';
 
 type Props = {};
@@ -11,42 +11,46 @@ type Props = {};
 export default class HomeView extends Component<Props> {
   render() {
     return (
-      <div className="x xd-column xj-spaceBetween mh-100vh">
-        <div style={{ backgroundColor: '#fc9377' }}>
-          <div className="mw-900 mh-auto ta-center c-white pv-5 ph-3 ph-0-m">
+      <div className="mh-100vh c-gray4">
+        <div className="x xd-column">
+          <div className="bgc-coral c-white ta-center pv-5 ph-5">
             <div className="mb-4">
               <img
                 src={poketoIntroUrl}
                 className="us-none ud-none pr-2 va-middle"
                 alt="Poketo"
-                width="160"
+                width="200"
               />
               <span className="br-4 ba-1 bc-lightGray ph-2 pv-1 ff-mono fs-12 ls-loose tt-uppercase">
                 Beta
               </span>
             </div>
-            <p className="fs-24">
-              A friendly manga tracker for following series you like.
-            </p>
             <div className="mv-4">
               <Button white inline>
                 Request an invite
               </Button>
             </div>
           </div>
-        </div>
-        <div className="mw-900 mh-auto pv-5 ph-3 ph-0-m">
-          <div className="x-m">
-            <div className="mb-4 mb-0-m">
-              <h3>Light and fun</h3>
+          <div className="mw-900 mh-auto ta-center pv-5 ph-3">
+            <p className="fs-24">
+              A friendly manga tracker for following series you like.
+            </p>
+          </div>
+          <div className="mw-900 mh-auto x xd-column xd-row-m xw-wrap">
+            <div className="w-50p-m ph-3 pv-3">
+              <h3 className="fw-medium">Light and fun</h3>
               <p>No ads, no downloads, no accounts. Simple.</p>
             </div>
-            <div className="mb-4 mb-0-m">
-              <h3>Easy reading</h3>
+            <div className="w-50p-m ph-3 pv-3">
+              <h3 className="fw-medium">No lock-in</h3>
+              <p>Import from and export to MyAnimeList and others.</p>
+            </div>
+            <div className="w-50p-m ph-3 pv-3">
+              <h3 className="fw-medium">Easy reading</h3>
               <p>Supports thousands of series from 8 sites (and counting).</p>
             </div>
-            <div className="mb-4 mb-0-m">
-              <h3>Open source</h3>
+            <div className="w-50p-m ph-3 pv-3">
+              <h3 className="fw-medium">Open source</h3>
               <p>
                 Built with open source; released as an{' '}
                 <a className="Link" href="https://github.com/poketo/site">
@@ -56,8 +60,20 @@ export default class HomeView extends Component<Props> {
               </p>
             </div>
           </div>
+          <div className="pv-5 fs-12 ta-center">
+            <Link to="/about" className="Link mr-3">
+              About
+            </Link>
+            <a
+              className="Link mr-3"
+              href="https://github.com/poketo/site/issues/new">
+              Send feedback
+            </a>
+            <a className="Link mr-3" href="https://github.com/poketo/site">
+              Source
+            </a>
+          </div>
         </div>
-        <Footer />
       </div>
     );
   }
