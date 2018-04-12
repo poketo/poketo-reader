49f00a2b014a3adc96347e724b68d94da8f9670f
Ross Zurowski <ross@rosszurowski.com>
2018-04-12 10:18:42 -0700

    Add a few more homepage styles

--- src/views/home-view.js
+++ src/views/home-view.js
@@ -12,11 +12,11 @@ export default class HomeView extends Component<Props> {
   render() {
     return (
       <div className="mh-100vh c-gray4 bgc-offwhite">
-        <div className="x xd-column pb-6">
+        <div className="x xd-column pb-5">
           <div className="x xd-column xj-center ta-center pv-5 ph-5 c-white bgc-coral mh-50vh">
             <div className="mb-4">
               <div className="mb-3">
-                <span className="br-4 ba-1 bc-lightGray ph-2 pv-1 ff-mono fs-12 ls-loose tt-uppercase">
+                <span className="br-4 bgc-fadedBlack ph-2 pv-1 fw-medium fs-12 ls-loose tt-uppercase ta-center">
                   Beta
                 </span>
               </div>
@@ -28,23 +28,23 @@ export default class HomeView extends Component<Props> {
               />
             </div>
           </div>
-          <div className="ta-center pv-5 ph-3 bgc-gray0">
+          <div className="ta-center pv-5 ph-3 bgc-lightGray">
             <div className="mw-900 mh-auto">
               <p className="fs-24">
                 A friendly manga tracker for following series you like.
               </p>
             </div>
           </div>
-          <div className="mw-900 mh-auto pv-5 x xd-column xd-row-m xw-wrap">
-            <div className="w-50p-m ph-3 pv-3">
+          <div className="mw-900 mh-auto pv-5 ta-center x xd-column xd-row-m xw-wrap">
+            <div className="w-33p-m ph-4 pv-3">
               <h3 className="fw-medium">Easy reading</h3>
               <p>Supports thousands of series from 8 sites (and counting).</p>
             </div>
-            <div className="w-50p-m ph-3 pv-3">
+            <div className="w-33p-m ph-4 pv-3">
               <h3 className="fw-medium">˗ˏˋ Light and fun ˎˊ˗</h3>
               <p>No ads, no downloads, no accounts. Simple.</p>
             </div>
-            <div className="w-50p-m ph-3 pv-3">
+            <div className="w-33p-m ph-4 pv-3">
               <h3 className="fw-medium">Open source</h3>
               <p>
                 Built with open source; released as an{' '}
@@ -56,11 +56,9 @@ export default class HomeView extends Component<Props> {
             </div>
           </div>
           <div className="p-fixed b-0 w-100p pv-4 ph-4 ta-center">
-            <Button>
-              Request an invite
-            </Button>
+            <Button>Request an invite</Button>
           </div>
-          <div className="pv-3 fs-14 ta-center">
+          <div className="pt-3 pb-5 fs-14 ta-center">
             <Link to="/about" className="Link mr-3">
               About
             </Link>
@@ -69,8 +67,8 @@ export default class HomeView extends Component<Props> {
               href="https://github.com/poketo/site/issues/new">
               Send feedback
             </a>
-            <a className="Link mr-3" href="https://github.com/poketo/site">
-              Source
+            <a className="Link mr-3" href="https://github.com/poketo">
+              Github
             </a>
           </div>
         </div>
