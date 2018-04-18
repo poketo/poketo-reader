8263ef6fe46948094cbc9b4acf0b0486c9af81bd
Ross Zurowski <ross@rosszurowski.com>
2018-04-18 23:59:39 +0300

    Add initial cache middleware
    Still need to deal with invalidations and cache clearing and versioning,
    but saving this as a start.

--- src/store/index.js
+++ src/store/index.js
@@ -2,16 +2,33 @@
 
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { getConfiguredCache } from 'money-clip';
 
+import persist from './middleware/persist';
 import reducer from './reducer';
 import api from '../api';
 
-const middleware = [thunk.withExtraArgument(api)];
+const MS_IN_HOURS = 60 * 1000;
 
-if (process.env.NODE_ENV === 'development') {
-  middleware.push(require('redux-logger').logger);
+export default function getStore(): Promise<{}> {
+  const cache = getConfiguredCache({
+    version: process.env.REACT_APP_COMMIT_REF || 'development',
+    maxAge: 1 * MS_IN_HOURS,
+  });
+
+  const middleware = [thunk.withExtraArgument(api), persist(cache)];
+
+  if (process.env.NODE_ENV === 'development') {
+    middleware.push(require('redux-logger').logger);
+  }
+
+  return cache.getAll().then(initialData => {
+    const store = createStore(
+      reducer,
+      initialData,
+      applyMiddleware(...middleware),
+    );
+
+    return store;
+  });
 }
-
-const store = createStore(reducer, applyMiddleware(...middleware));
-
-export default store;
---
+++ src/store/middleware/persist.js
@@ -0,0 +1,25 @@
+// @flow
+
+import getPersistMiddleware from 'redux-persist-middleware';
+
+const actionMap: { [string]: string[] } = {
+  ADD_ENTITIES: ['series', 'collection'],
+};
+
+type Cache = {
+  set: (key: string, value: string) => Promise<void>,
+  get: (key: string, value: string) => Promise<void>,
+};
+
+/**
+ * Local caching middleware
+ *
+ * This middleware intercepts any actions that modify entities on the local DB
+ * and caches them to LocalStorage or IndexedDB.
+ */
+
+export default (cache: Cache) =>
+  getPersistMiddleware({
+    cacheFn: cache.set,
+    actionMap,
+  });
