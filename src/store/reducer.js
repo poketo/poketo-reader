5431ea07f8845f40768ba9263e38756cd600d91c
Ross Zurowski <ross@rosszurowski.com>
2018-05-10 16:58:42 -0700

    Remove meta reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -4,12 +4,10 @@ import { combineReducers } from 'redux';
 
 import chapters from './reducers/chapters';
 import collections from './reducers/collections';
-import meta from './reducers/meta';
 import series from './reducers/series';
 
 export default combineReducers({
   collections,
   chapters,
-  meta,
   series,
 });
