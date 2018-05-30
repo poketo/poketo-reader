3a9025d7bb5526ad88e16cff2847819e06bb1951
Ross Zurowski <ross@rosszurowski.com>
2018-05-29 21:41:41 -0700

    Fix apostrophe side

--- src/containers/feedback-form.js
+++ src/containers/feedback-form.js
@@ -64,7 +64,7 @@ export default class FeedbackForm extends Component<Props, State> {
     return (
       <form onSubmit={this.handleSubmit}>
         <p className="mb-3">
-          We‘d love to hear any feedback you have about Poketo. You can also{' '}
+          We’d love to hear any feedback you have about Poketo. You can also{' '}
           <a
             className="Link"
             href={`mailto:${config.email}`}
