a907e4412d244f2cf5e9792930547c981b165fbd
Ross Zurowski <ross@rosszurowski.com>
2018-06-01 09:30:20 -0700

    Set explicit cache version
    Setting it based on the commit hash resulted in any changes to any part
    of the codebase clearing the cache, which is a bit of an annoying
    behaviour.

--- src/store/cache.js
+++ src/store/cache.js
@@ -3,8 +3,12 @@
 import { getConfiguredCache } from 'money-clip';
 import ms from 'milliseconds';
 
+// NOTE: this cache version should be bumped any time the schema of the store
+// changes shape. It'll invalidate the cached data and force a re-fetch.
+const version = '1527870408489';
+
 const cache = getConfiguredCache({
-  version: process.env.REACT_APP_COMMIT_REF || 'development',
+  version,
   maxAge: ms.days(5),
 });
 
