26735d55bee3bf027fea98416fe6023c901167f9
Ross Zurowski <ross@rosszurowski.com>
2018-06-07 09:42:08 -0700

    Bump cache API version

--- src/store/cache.js
+++ src/store/cache.js
@@ -6,7 +6,7 @@ import ms from 'milliseconds';
 // NOTE: this cache version should be bumped any time the schema of the store
 // changes shape, or the API responses change. It'll invalidate cached data and
 // force a re-fetch.
-const apiVersion = 'jkqtgajukx';
+const apiVersion = 'rxmvmpsuta';
 const storeVersion = '1527870408490';
 const version = `${apiVersion}-${storeVersion}`;
 
