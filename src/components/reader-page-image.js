c9c6ed1deed7daefea9492263b4ccf44d298508b
Ross Zurowski <ross@rosszurowski.com>
2018-06-09 00:09:31 -0700

    Add retry to PageImage component
    When the network fails, so far images just display a blank space without
    an opportunity to reload the image without reloading the page. This
    commit introduces an image state to reload the image if it fails.

--- src/components/reader-page-image.js
+++ src/components/reader-page-image.js
@@ -1,15 +1,17 @@
 // @flow
 
-import React from 'react';
+import React, { PureComponent } from 'react';
 import classNames from 'classnames';
+import Button from './button';
+import Icon from './icon';
 import './reader-page-image.css';
 
 if (typeof global.window !== 'undefined') {
   global.window.lazySizesConfig = window.lazySizesConfig || {};
   global.window.lazySizesConfig.lazyClass = 'js-lazysizes';
-  global.window.lazySizesConfig.preloadClass = 'js-lazysizes-preload';
+  global.window.lazySizesConfig.loadingClass = 'js-lazysizes-loading';
   global.window.lazySizesConfig.loadedClass = 'js-lazysizes-loaded';
-  global.window.lazySizesConfig.expFactor = 1200;
+  global.window.lazySizesConfig.preloadClass = 'js-lazysizes-preload';
   global.window.lazySizesConfig.expFactor = 2.0;
 
   require('lazysizes/plugins/attrchange/ls.attrchange.js');
@@ -23,38 +25,68 @@ type Props = {
     width: number,
     height: number,
   },
-  preload: boolean,
-  fitHeight: boolean,
 };
 
-const SeriesPageImage = ({ page, preload, fitHeight }: Props) => {
-  const hasSize = page.width && page.height;
-
-  return (
-    <span
-      className={classNames('PageImage', {
-        'PageImage--hasSize': hasSize,
-      })}>
-      <img
-        src="data:image/gif;base64,R0lGODlhAQABAPAAAPLy8v///yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="
-        data-src={page.url}
-        alt={page.alt}
-        className={`PageImage-image js-lazysizes ${
-          preload ? 'js-lazysizes-preload' : ''
-        }`}
-      />
-      <canvas
-        className="PageImage-background"
-        width={page.width || 800}
-        height={page.height || 800}
-      />
-    </span>
-  );
+type State = {
+  hasError: boolean,
 };
 
-SeriesPageImage.defaultProps = {
-  preload: false,
-  fitHeight: false,
-};
+const DEFAULT_WIDTH = 800;
+const DEFAULT_HEIGHT = 1050;
 
-export default SeriesPageImage;
+export default class ReaderPageImage extends PureComponent<Props, State> {
+  state = {
+    hasError: false,
+  };
+
+  handleError = (err: SyntheticEvent<HTMLImageElement>) => {
+    this.setState({ hasError: true });
+  };
+
+  handleLoad = () => {
+    this.setState({ hasError: false });
+  };
+
+  handleRetryClick = (e: SyntheticMouseEvent<HTMLButtonElement>) => {
+    this.setState({ hasError: false });
+  };
+
+  render() {
+    const { page } = this.props;
+    const { hasError } = this.state;
+    const { width = DEFAULT_WIDTH, height = DEFAULT_HEIGHT } = page;
+
+    const hasSize = Boolean(page.width) && Boolean(page.height);
+
+    return (
+      <span
+        className={classNames('PageImage', {
+          'PageImage--hasSize': hasSize,
+          'PageImage--hasError': hasError,
+        })}>
+        {hasError ? (
+          <div className="p-absolute p-center">
+            <Button noPadding inline onClick={this.handleRetryClick}>
+              <Icon name="refresh" iconSize={16} size={44} />
+              <span className="pr-3">Retry</span>
+            </Button>
+          </div>
+        ) : (
+          <img
+            src="data:image/gif;base64,R0lGODlhAQABAPAAAPLy8v///yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="
+            data-src={page.url}
+            alt={page.alt}
+            onError={this.handleError}
+            onLoad={this.handleLoad}
+            className="PageImage-image js-lazysizes"
+          />
+        )}
+        <canvas
+          className="PageImage-background bgc-gray3"
+          width={width}
+          height={height}
+        />
+      </span>
+    );
+  }
+}
