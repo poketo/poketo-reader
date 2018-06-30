8a4b5e73141ffde50a89de3cf1d975555314b6b4
Ross Zurowski <ross@rosszurowski.com>
2018-06-30 10:47:40 -0700

    Move chapter picker to panel approach

--- src/components/reader-navigation.js
+++ src/components/reader-navigation.js
@@ -1,9 +1,9 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
 
 import Icon from '../components/icon';
-import Popover from '../components/popover';
+import Panel from '../components/panel';
 import ReaderChapterPicker from '../components/reader-chapter-picker';
 import ReaderChapterLink from '../components/reader-chapter-link';
 import utils from '../utils';
@@ -11,58 +11,84 @@ import utils from '../utils';
 import type { Chapter, Series } from '../types';
 
 type Props = {
-  isBottom: boolean,
   chapter: Chapter,
   collectionSlug: ?string,
   onChapterSelectChange: (e: SyntheticInputEvent<HTMLSelectElement>) => void,
   seriesChapters: Series,
 };
 
-const ReaderNavigation = ({
-  isBottom,
-  chapter,
-  collectionSlug,
-  onChapterSelectChange,
-  seriesChapters,
-}: Props) => {
-  const chapterIndex = seriesChapters.findIndex(c => c.id === chapter.id);
-  const previousChapter = seriesChapters[chapterIndex + 1] || null;
-  const nextChapter = seriesChapters[chapterIndex - 1] || null;
+type State = {
+  showingPanel: boolean,
+};
 
-  return (
-    <nav className="p-relative c-white x xa-center xj-spaceBetween mw-500 mh-auto pv-2 ph-3">
-      <div className="z-2">
-        <ReaderChapterLink
-          collectionSlug={collectionSlug}
-          chapter={previousChapter}>
-          <Icon name="direct-left" />
-        </ReaderChapterLink>
-      </div>
-      <Popover
-        content={({ close }) => (
+export default class ReaderNavigation extends Component<Props, State> {
+  state = {
+    showingPanel: false,
+  };
+
+  handlePickerClick = () => {
+    this.setState({ showingPanel: true });
+  };
+
+  handlePickerPanelClose = () => {
+    this.setState({ showingPanel: false });
+  };
+
+  renderPickerPanel() {
+    const { chapter, onChapterSelectChange, seriesChapters } = this.props;
+
+    if (this.state.showingPanel === false) {
+      return null;
+    }
+
+    return (
+      <Panel.Transition>
+        <Panel onRequestClose={this.handlePickerPanelClose}>
           <div
+            className="pt-3 pb-4"
             style={{
               overflowY: 'scroll',
               WebkitOverflowScrolling: 'touch',
-              minWidth: 240,
-              maxWidth: '90vw',
-              maxHeight: 400,
-              zIndex: 9,
+              maxHeight: '60vh',
             }}>
+            <Panel.Title className="ph-3 mb-2">Pick a chapter</Panel.Title>
             <ReaderChapterPicker
               chapter={chapter}
               seriesChapters={seriesChapters}
               onChapterClick={chapter => {
                 onChapterSelectChange(chapter);
-                close();
+                this.handlePickerPanelClose();
               }}
             />
           </div>
-        )}
-        position={isBottom ? Popover.Position.TOP : Popover.Position.BOTTOM}>
+        </Panel>
+      </Panel.Transition>
+    );
+  }
+
+  render() {
+    const { chapter, collectionSlug, seriesChapters } = this.props;
+
+    const chapterIndex = seriesChapters.findIndex(c => c.id === chapter.id);
+    const previousChapter = seriesChapters[chapterIndex + 1] || null;
+    const nextChapter = seriesChapters[chapterIndex - 1] || null;
+
+    return (
+      <nav className="p-relative c-white x xa-center xj-spaceBetween mw-500 mh-auto pv-2 ph-3">
+        <div className="z-2">
+          <ReaderChapterLink
+            collectionSlug={collectionSlug}
+            chapter={previousChapter}>
+            <Icon name="direct-left" />
+          </ReaderChapterLink>
+          <Panel.TransitionGroup>
+            {this.renderPickerPanel()}
+          </Panel.TransitionGroup>
+        </div>
         <a
           className="PillLink pv-2 ph-3 d-inlineBlock c-white c-pointer ta-center"
-          style={{ lineHeight: '1.25' }}>
+          style={{ lineHeight: '1.25' }}
+          onClick={this.handlePickerClick}>
           <div className="x xa-center xj-center" style={{ lineHeight: '24px' }}>
             <span className="ml-1 mr-2">{utils.getChapterLabel(chapter)}</span>
             <Icon name="direct-down" size={18} iconSize={18} />
@@ -73,16 +99,14 @@ const ReaderNavigation = ({
             </div>
           )}
         </a>
-      </Popover>
-      <div className="z-2">
-        <ReaderChapterLink
-          collectionSlug={collectionSlug}
-          chapter={nextChapter}>
-          <Icon name="direct-right" />
-        </ReaderChapterLink>
-      </div>
-    </nav>
-  );
-};
-
-export default ReaderNavigation;
+        <div className="z-2">
+          <ReaderChapterLink
+            collectionSlug={collectionSlug}
+            chapter={nextChapter}>
+            <Icon name="direct-right" />
+          </ReaderChapterLink>
+        </div>
+      </nav>
+    );
+  }
+}
