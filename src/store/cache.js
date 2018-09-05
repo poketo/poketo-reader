d228e1c0c593a218f90ac099e5ec90886d557a9e
Ross Zurowski <ross@rosszurowski.com>
2018-09-04 19:20:57 -0700

    Bump store cache version

--- src/store/cache.js
+++ src/store/cache.js
@@ -5,8 +5,8 @@ import { getConfiguredCache } from 'money-clip';
 // NOTE: this cache version should be bumped any time the schema of the store
 // changes shape, or the API responses change. It'll invalidate cached data and
 // force a re-fetch.
-const apiVersion = 'rxmvmpsuta';
-const storeVersion = '1530681927316';
+const apiVersion = 'pqrtebuhhk';
+const storeVersion = '1536114047316';
 const version = `${apiVersion}-${storeVersion}`;
 const maxAge = 432000000; // 5 days in milliseconds
 
