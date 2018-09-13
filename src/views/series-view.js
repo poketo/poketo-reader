f60a696c24bcba543f1555bcf00490c48496b730
Ross Zurowski <ross@rosszurowski.com>
2018-09-12 18:18:21 -0700

    Refine series view

--- src/views/series-view.js
+++ src/views/series-view.js
@@ -53,6 +53,15 @@ class SeriesPageContainer extends Component<ContainerProps> {
   }
 }
 
+const Label = ({ className, ...props }: { className?: string }) => (
+  <div className={cx(className, 'fs-14 c-gray3 mb-1')} {...props} />
+);
+
+const iconProps = {
+  iconSize: 20,
+  size: 44,
+};
+
 type Props = {
   series: Series,
   chapters: Chapter[],
@@ -66,15 +75,6 @@ type Props = {
   unreadChapterCount: number,
 };
 
-const Label = ({ className, ...props }: { className?: string }) => (
-  <div className={cx(className, 'fs-14 c-gray3 mb-1')} {...props} />
-);
-
-const iconProps = {
-  iconSize: 20,
-  size: 44,
-};
-
 const SeriesPage = ({
   series,
   chapters,
@@ -82,118 +82,135 @@ const SeriesPage = ({
   lastReadChapterId,
   markAsRead,
   unreadChapterCount,
-}: Props) => (
-  <div className="pb-5">
-    <ScrollReset />
-    <div className="mw-600 w-100p mh-auto p-relative">
-      <header className="x xa-center xj-spaceBetween pv-3 mb-3 c-white p-absolute t-0 w-100p z-3">
-        <Link to="/" className="x hover">
-          <Icon name="arrow-left" {...iconProps} />
-        </Link>
-        <Popover
-          content={({ close }) => (
-            <div className="pa-2">
-              <Popover.Item
-                iconBefore={<Icon name="new-tab" {...iconProps} />}
-                label={`Open on ${series.site.name}`}
-                href={series.url}
-                onClick={close}
-                target="_blank"
-                rel="noreferrer noopener"
-              />
-              {collectionSlug &&
-                unreadChapterCount > 0 && (
-                  <Fragment>
-                    <Popover.Divider />
-                    <Popover.Item
-                      iconBefore={<Icon name="bookmark" {...iconProps} />}
-                      label={`Mark ${unreadChapterCount} chapters as read`}
-                      onClick={() => {
-                        const latestChapter = chapters[0];
-                        markAsRead(collectionSlug, series.id, latestChapter.id);
-                        close();
-                      }}
-                    />
-                  </Fragment>
-                )}
+}: Props) => {
+  const firstChapter = chapters[0];
+  const lastReadChapter =
+    unreadChapterCount > 0 && lastReadChapterId
+      ? chapters.find(c => c.id === lastReadChapterId)
+      : null;
+
+  return (
+    <div className="pb-5">
+      <ScrollReset />
+      <div className="mw-600 w-100p mh-auto p-relative">
+        <header className="x xa-center xj-spaceBetween pv-3 mb-3 c-white p-absolute t-0 w-100p z-3">
+          <Link to="/" className="x hover">
+            <Icon name="arrow-left" {...iconProps} />
+          </Link>
+          <Popover
+            content={({ close }) => (
+              <div className="pa-2">
+                <Popover.Item
+                  iconBefore={<Icon name="new-tab" {...iconProps} />}
+                  label={`Open on ${series.site.name}`}
+                  href={series.url}
+                  onClick={close}
+                  target="_blank"
+                  rel="noreferrer noopener"
+                />
+                {collectionSlug &&
+                  unreadChapterCount > 0 && (
+                    <Fragment>
+                      <Popover.Divider />
+                      <Popover.Item
+                        iconBefore={<Icon name="bookmark" {...iconProps} />}
+                        label={`Mark ${unreadChapterCount} chapters as read`}
+                        onClick={() => {
+                          const latestChapter = chapters[0];
+                          markAsRead(
+                            collectionSlug,
+                            series.id,
+                            latestChapter.id,
+                          );
+                          close();
+                        }}
+                      />
+                    </Fragment>
+                  )}
+              </div>
+            )}
+            position={Popover.Position.BOTTOM_RIGHT}>
+            <Button inline noPadding className="x hover">
+              <Icon name="more-vertical" {...iconProps} />
+            </Button>
+          </Popover>
+        </header>
+      </div>
+      <div className="bgc-black w-100p" style={{ height: 140 }} />
+      <div className="mw-600 mh-auto p-relative">
+        <header className="x xa-end mb-4 ph-3" css="margin-top: -60px;">
+          <div className="mr-3 w-50p" css="max-width: 140px;">
+            <a
+              href={series.coverImageUrl}
+              target="_blank"
+              rel="noreferrer noopener">
+              <CoverImage series={series} />
+            </a>
+          </div>
+          <div>
+            <h1 className="fs-24 fs-32-m fw-semibold lh-1d25">
+              {series.title}
+            </h1>
+            <a
+              href={series.url}
+              className="c-gray3"
+              target="_blank"
+              rel="noopener noreferrer">
+              {series.site.name}
+              <Icon name="new-tab" size={16} iconSize={16} />
+            </a>
+          </div>
+        </header>
+        <div className="ph-3 mb-4">
+          {collectionSlug && (
+            <div className="mb-4">
+              <FollowButton seriesId={series.id} />
             </div>
           )}
-          position={Popover.Position.BOTTOM_RIGHT}>
-          <Button inline noPadding className="x hover">
-            <Icon name="more-vertical" {...iconProps} />
-          </Button>
-        </Popover>
-      </header>
-    </div>
-    <div className="bgc-black w-100p" style={{ height: 140 }} />
-    <div className="mw-600 mh-auto p-relative">
-      <header className="x xa-end mb-4 ph-3" css="margin-top: -60px;">
-        <div className="mr-3 w-50p" css="max-width: 140px;">
-          <a
-            href={series.coverImageUrl}
-            target="_blank"
-            rel="noreferrer noopener">
-            <CoverImage series={series} />
-          </a>
-        </div>
-        <div>
-          <h1 className="fs-24 fs-32-m fw-semibold lh-1d25">{series.title}</h1>
-          <a
-            href={series.url}
-            className="c-gray3"
-            target="_blank"
-            rel="noopener noreferrer">
-            {series.site.name}
-            <Icon name="new-tab" size={16} iconSize={16} />
-          </a>
-        </div>
-      </header>
-      <div className="ph-3 mb-4">
-        {collectionSlug && (
-          <div className="mb-4">
-            <FollowButton seriesId={series.id} />
+          <div className="mb-3">
+            <Label>Author</Label>
+            <div>{series.author}</div>
           </div>
-        )}
-        <div className="mb-3">
-          <Label>Author</Label>
-          <div>{series.author}</div>
+          <div>
+            <Label>Description</Label>
+            <div>{series.description}</div>
+          </div>
         </div>
-        <div>
-          <Label>Description</Label>
-          <div>{series.description}</div>
-        </div>
-      </div>
-      {unreadChapterCount > 0 &&
-        lastReadChapterId && (
-          <div className="ph-3 mb-4">
-            <Link to={utils.getReaderUrl(lastReadChapterId)}>
+        <div className="ph-3 mb-4">
+          {lastReadChapter ? (
+            <Link to={utils.getReaderUrl(lastReadChapter.id)}>
               <Button>Continue Reading</Button>
             </Link>
-          </div>
-        )}
-      <div>
-        {series.supportsReading ? (
-          chapters.map(chapter => (
-            <div className="bb-1 bc-gray1 ph-3" key={chapter.id}>
-              <ChapterRow
-                chapter={chapter}
-                isLastReadChapter={chapter.id === lastReadChapterId}
-              />
+          ) : (
+            <Link to={utils.getReaderUrl(firstChapter)}>
+              <Button>Read</Button>
+            </Link>
+          )}
+        </div>
+        <div>
+          {series.supportsReading ? (
+            chapters.map(chapter => (
+              <div className="bb-1 bc-gray1 ph-3" key={chapter.id}>
+                <ChapterRow
+                  chapter={chapter}
+                  isLastReadChapter={chapter.id === lastReadChapterId}
+                />
+              </div>
+            ))
+          ) : (
+            <div className="bgc-gray0 ta-center br-3 ph-4 pv-4">
+              <Icon name="warning" className="mb-3" />
+              <div className="mb-3 fw-semibold">No chapters</div>
+              <p className="c-gray4">
+                {series.site.name} doesn't support reading on Poketo.
+              </p>
             </div>
-          ))
-        ) : (
-          <div className="bgc-gray0 ta-center br-3 ph-4 pv-4">
-            <Icon name="warning" className="mb-3" />
-            <div className="mb-3 fw-semibold">No chapters</div>
-            <p className="c-gray4">
-              {series.site.name} doesn't support reading on Poketo.
-            </p>
-          </div>
-        )}
+          )}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 const mapStateToProps = (state, ownProps) => {
   const { series: seriesById, chapters: chaptersById } = state;
